6f00b2ce4efeabc9e7479299fb37a527b38f54e1
huangyan <1621740748@qq.com>
2020-08-24 21:52:02 +0800

    提交

--- src/test/java/com/example/aws/elasticsearch/demo/ApplicationTests.java
+++ src/test/java/com/example/aws/elasticsearch/demo/ApplicationTests.java
@@ -10,7 +10,6 @@ import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
-import org.elasticsearch.search.aggregations.metrics.ValueCount;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
@@ -37,8 +35,8 @@ public class ApplicationTests {
 	       SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 	       TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_technology")
 	               .field("technologies.name.keyword");   //text类型不能用于索引或排序，必须转成keyword类型
-	       aggregation.subAggregation(AggregationBuilders.count("tCount")
-	               .field("technologies.name.keyword"));  //avg_age 为子聚合名称，名称可随意
+//	       aggregation.subAggregation(AggregationBuilders.count("tCount")
+//	               .field("technologies.name.keyword"));  //avg_age 为子聚合名称，名称可随意
 	       searchSourceBuilder.aggregation(aggregation);
 	       searchRequest.source(searchSourceBuilder);
 	       SearchResponse searchResponse = null;
@@ -50,15 +48,23 @@ public class ApplicationTests {
 	       Aggregations aggregations = searchResponse.getAggregations();
 
 	       Terms byCompanyAggregation = aggregations.get("by_technology");
-	       Terms.Bucket elasticBucket = byCompanyAggregation.getBucketByKey("java1");
-//	       try {
-//			System.out.println(objectMapper.writeValueAsString(elasticBucket));
-//		} catch (JsonProcessingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	       ValueCount averageAge = elasticBucket.getAggregations().get("tCount");
-	       double avg = averageAge.getValue();
-	       System.out.println("java 人数："+avg);
+//	       Terms.Bucket elasticBucket = byCompanyAggregation.getBucketByKey("java1");
+////	       try {
+////			System.out.println(objectMapper.writeValueAsString(elasticBucket));
+////		} catch (JsonProcessingException e) {
+////			// TODO Auto-generated catch block
+////			e.printStackTrace();
+////		}
+//	       ValueCount averageAge = elasticBucket.getAggregations().get("tCount");
+//	       double avg = averageAge.getValue();
+//	       System.out.println("java 人数："+avg);
+	      for(Terms.Bucket bucket: byCompanyAggregation.getBuckets()) {
+	    	  String name=bucket.getKeyAsString();
+	    	  Long count=bucket.getDocCount();
+	    	  System.out.println("------------------------");
+	    	  System.out.println("name:"+name);
+	    	  System.out.println("count:"+count);
+	    	  
+	      }
 	}
 }
